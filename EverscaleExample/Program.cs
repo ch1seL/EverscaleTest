fa08a54aeec421019fc67c281cdec219b7b19025
Alexander Salamatov <ch1seL@ya.ru>
2021-11-26 16:36:01 +0300

    fix

--- EverscaleExample/Program.cs
+++ EverscaleExample/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ch1seL.TonNet.Abstract;
+using ch1seL.TonNet.Adapter.Rust;
+using ch1seL.TonNet.Client;
+using ch1seL.TonNet.Client.Models;
+using Microsoft.Extensions.Options;
+
+ITonClient tonClient = new TonClient(new TonClientRustAdapter(Options.Create(new TonClientOptions())));
 Console.WriteLine("Hello, World!");
+Console.WriteLine((await tonClient.Crypto.MnemonicFromRandom(new ParamsOfMnemonicFromRandom())).Phrase);
