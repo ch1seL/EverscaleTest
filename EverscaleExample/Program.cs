ea5cc7b40693b234c5216eeb77efe3cea364badc
Alexander Salamatov <ch1seL@ya.ru>
2022-01-22 21:33:00 +0300

    migrate from TonClient to EverClient

--- EverscaleExample/Program.cs
+++ EverscaleExample/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using ch1seL.TonNet.Abstract;
-using ch1seL.TonNet.Adapter.Rust;
-using ch1seL.TonNet.Client;
-using ch1seL.TonNet.Client.Models;
+using EverscaleNet.Abstract;
+using EverscaleNet.Adapter.Rust;
+using EverscaleNet.Client;
+using EverscaleNet.Client.Models;
+using EverscaleNet.Models;
 using Microsoft.Extensions.Options;
 
-ITonClient tonClient = new TonClient(new TonClientRustAdapter(Options.Create(new TonClientOptions())));
+IEverClient tonClient = new EverClient(new EverClientRustAdapter(Options.Create(new EverClientOptions())));
 Console.WriteLine("Hello, World!");
 Console.WriteLine((await tonClient.Crypto.MnemonicFromRandom(new ParamsOfMnemonicFromRandom())).Phrase);
